9649644ae4c4fdef7ebfdc997f8d8e503c79dfbb
Neil Mrva <neil.mrva@gmail.com>
2018-11-20 09:46:02 -0500

    - Register user and proceed to chat

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,38 +6,36 @@
 //
 
 import UIKit
+import Firebase
 
-
-class RegisterViewController: UIViewController {
-
-    
-    //Pre-linked IBOutlets
-
+class RegisterViewController: UIViewController
+{
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
-    
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-
-  
-    @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
+    @IBAction func registerPressed(_ sender: AnyObject)
+    {
+        let email = emailTextfield.text!
+        let password = passwordTextfield.text!
         
         //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
-    } 
-    
-    
+        Auth.auth().createUser(withEmail: email, password: password)
+        {
+            (result, error) in
+            
+            if let error = error
+            {
+                print(error)
+            }
+            else
+            {
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+    }
 }
