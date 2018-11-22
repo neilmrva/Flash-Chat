6c97905e81b4a9f46dac9f3cd6f6f470278eb2b8
Neil Mrva <neil.mrva@gmail.com>
2018-11-21 19:34:37 -0500

    - Add variables to message model - Enable textfield delegate and animation

--- Flash Chat/Message.swift
+++ Flash Chat/Message.swift
@@ -4,10 +4,10 @@
 //
 //  This is the model class that represents the blueprint for a message
 
-class Message {
-    
+class Message
+{
     //TODO: Messages need a messageBody and a sender variable
-    
-    
+    var messageBody:String = ""
+    var sender:String = ""
     
 }
